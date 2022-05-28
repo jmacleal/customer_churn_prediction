d2c0d6863d914ffaa7e808daad9e8a38ecc3b203
J. MARCOS LEAL B. FILHO <104634914+jmacleal@users.noreply.github.com>
2022-05-28 11:00:57 -0300

    delete unused folder

--- source/hello_world/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Instantiate the app.
-app = FastAPI()
-
-# Define a GET on the specified endpoint.
-@app.get("/")
-async def say_hello():
-    return {"greeting": "Hello World!"}
